8c8ba3a823bfe4d11548fc77b9e005923fc8c82c
devhoss <velasco2288@gmail.com>
2023-10-22 13:40:04 -0500

    seek y assign

--- src/main/java/com/devhoss/kafka/assign/seek/AssignSeekConsumer.java
+++ src/main/java/com/devhoss/kafka/assign/seek/AssignSeekConsumer.java
@@ -1,4 +1,4 @@
-package com.devhoss.kafka.consumer;
+package com.devhoss.kafka.assign.seek;
 
 import java.time.Duration;
 import java.util.Arrays;
@@ -10,10 +10,10 @@ import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.TopicPartition;
 import org.slf4j.LoggerFactory;
 
-public class ConsumerKafka {
+public class AssignSeekConsumer {
 
 	// Create a Logger
-	public static final org.slf4j.Logger log = LoggerFactory.getLogger(ConsumerKafka.class);
+	public static final org.slf4j.Logger log = LoggerFactory.getLogger(AssignSeekConsumer.class);
 
 	public static void main(String[] args) {
 
@@ -26,20 +26,20 @@ public class ConsumerKafka {
 		props.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
 
 		try (KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);) {
-			consumer.subscribe(Arrays.asList("testtopicreplication3"));
-			
-			//TopicPartition topicPartition = new TopicPartition("testtopicreplication3", 0);
-			//		consumer.assign(Arrays.asList(topicPartition));
-			//		consumer.seek(topicPartition, 12);
+			//consumer.subscribe(Arrays.asList("testtopicreplication3"));
+			TopicPartition topicPartition = new TopicPartition("testtopicreplication3", 1);// es la particion que vamos a leer
+			consumer.assign(Arrays.asList(topicPartition));
+			consumer.seek(topicPartition, 76830);// la posicion de la particion de donde se desea leer
+
 
 			while (true) {
 				ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
 
 				for (ConsumerRecord<String, String> record : records) 
-					log.info("partition = {} ,offset = {}, key = {}, value ={}",record.partition(), record.offset(),record.key(),record.value());
-					//System.out.println( "*******************offset = " + record.offset() + "key = " + record.key() + "value = " + record.value());
-					
-				
+					log.info("partition50 = {} ,offset = {}, key = {}, value ={}",record.partition(), record.offset(),record.key(),record.value());
+				//System.out.println( "*******************offset = " + record.offset() + "key = " + record.key() + "value = " + record.value());
+
+
 			}
 		}
 
