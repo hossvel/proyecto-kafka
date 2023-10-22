90977abe3c25e5122f5f0d7b1b94bd2e604a3a2d
devhoss <velasco2288@gmail.com>
2023-10-22 12:48:50 -0500

    callback

---
+++ src/main/java/com/devhoss/kafka/callback/CallbackConsumer.java
@@ -0,0 +1,41 @@
+package com.devhoss.kafka.callback;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.slf4j.LoggerFactory;
+
+public class CallbackConsumer {
+
+	// Create a Logger
+	public static final org.slf4j.Logger log = LoggerFactory.getLogger(CallbackConsumer.class);
+
+	public static void main(String[] args) {
+
+		Properties props = new Properties();
+		props.setProperty("bootstrap.servers", "localhost:9092");
+		props.setProperty("group.id", "devhoss-group");
+		props.setProperty("enable.auto.commit", "true");
+		props.setProperty("auto.commit.interval.ms", "1000");
+		props.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		props.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+
+		try (KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);) {
+			consumer.subscribe(Arrays.asList("testtopicreplication3"));
+			while (true) {
+				ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+
+				for (ConsumerRecord<String, String> record : records) 
+					log.info("partition = {} ,offset = {}, key = {}, value ={}",record.partition(), record.offset(),record.key(),record.value());
+					//System.out.println( "*******************offset = " + record.offset() + "key = " + record.key() + "value = " + record.value());
+					
+				
+			}
+		}
+
+	}
+}
---
+++ src/main/java/com/devhoss/kafka/callback/CallbackLambdaProducer.java
@@ -0,0 +1,49 @@
+package com.devhoss.kafka.callback;
+
+import java.util.Properties;
+
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.slf4j.LoggerFactory;
+
+
+public class CallbackLambdaProducer {
+
+	public static final org.slf4j.Logger log = LoggerFactory.getLogger(CallbackLambdaProducer.class);
+
+	public static void main(String[] args) {
+		long inicio = System.currentTimeMillis();
+
+		Properties props=new Properties();
+		props.put("bootstrap.servers","localhost:9093");
+		props.put("acks","1");// SE NESESITA QUE POR LO MENOS UN NODO NOS RESPONDA QUE RECIBIO
+		props.put("key.serializer","org.apache.kafka.common.serialization.StringSerializer");
+		props.put("value.serializer","org.apache.kafka.common.serialization.StringSerializer");
+		props.put("linger.ms","5");
+
+		try(Producer<String, String>producer=new KafkaProducer<>(props);) {
+
+			for(int i= 0; i < 10000;i++) {
+				//producer.send(new ProducerRecord<>("testtopicreplication3",String.valueOf(i),"Mensaje - " + (i + 1)));
+
+				producer.send(new ProducerRecord<>("testtopicreplication3",String.valueOf(i),"Mensaje - " + (i + 1)), (metadata,exception) -> {
+					if(exception!=null) {
+						log.error("Error sending the message ", exception);
+					}
+					log.info("Partition = {} Offset ={}", metadata.partition(),metadata.offset());
+				});
+
+
+
+
+			}
+			producer.flush();
+		}
+
+		System.out.println("Tiempo de procesamiento: "  + (System.currentTimeMillis() - inicio) + " ms");
+	}
+
+
+}
